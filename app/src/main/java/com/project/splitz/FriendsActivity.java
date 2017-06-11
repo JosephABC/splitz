e64dbf798fbba630aec7f5d523105595cc3535d9
Nicole Tann <tannlilin@gmail.com>
2017-06-12 01:41:29 +0800

    a

--- app/src/main/java/com/project/splitz/FriendsActivity.java
+++ app/src/main/java/com/project/splitz/FriendsActivity.java
@@ -1,5 +1,6 @@
 package com.project.splitz;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FriendsActivity extends AppCompatActivity implements View.OnClickListener {
+public class FriendsActivity extends Activity implements View.OnClickListener {
 
     public FirebaseAuth mAuth;
     private ArrayAdapter<String> adapter;
@@ -32,23 +33,29 @@ public class FriendsActivity extends AppCompatActivity implements View.OnClickLi
 
         mAuth = FirebaseAuth.getInstance();
 
+
+
         // Retrieve friend list
         FirebaseUser currentUser = mAuth.getCurrentUser();
         final String currentUid = currentUser.getUid();
-        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users").child(currentUid).child("Friends");
-        Query UserQuery = mDatabase;
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        Query UserQuery = mDatabase.child(currentUid).child("Friends");
         UserQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // Generate list of friends details
                 ArrayList<String> arrayList = new ArrayList<String>();
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
                     String Entry = child.getValue().toString();
                     arrayList.add(Entry);
-                    //Friends View
-                    ListView friendList = (ListView) findViewById(R.id.listViewFriends);
-                    adapter = new ArrayAdapter<String>(FriendsActivity.this, android.R.layout.simple_list_item_1, arrayList);
-                    friendList.setAdapter(adapter);
                 }
+
+                    //Friend List Adapter
+                    MyAdapter adapter = new MyAdapter(FriendsActivity.this, generateData());
+                    ListView friendList = (ListView) findViewById(R.id.listViewFriends);
+                    friendList.setAdapter(adapter);
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -57,6 +64,16 @@ public class FriendsActivity extends AppCompatActivity implements View.OnClickLi
 
 
         findViewById(R.id.AddFriendBtn).setOnClickListener(this);
+    }
+
+    // Example of creating ArrayList of "items"
+    private ArrayList<Items> generateData(){
+        ArrayList<Items> items = new ArrayList<Items>();
+        items.add(new Items("Item 1","First Item on the list"));
+        items.add(new Items("Item 2","Second Item on the list"));
+        items.add(new Items("Item 3","Third Item on the list"));
+
+        return items;
     }
 
 
---
+++ app/src/main/java/com/project/splitz/Items.java
@@ -0,0 +1,23 @@
+package com.project.splitz;
+
+public class Items {
+
+    private String title;
+    private String description;
+
+    public Items(String title, String description) {
+        super();
+        this.title = title;
+        this.description = description;
+    }
+
+    // getters and setters...
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
